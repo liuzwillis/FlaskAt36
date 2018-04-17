e0d81dc0bafcf570cefcaa2fd87969d271781867
willis <liuzw3@163.com>
2018-04-17 20:18:28 +0800

    ch2 some changes

--- flasky.py
+++ flasky.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
+# -*-coding:utf-8 -*-
 
 from flask import Flask
 from flask import request
 from flask import make_response
-
-# Server用于指定server的host等参数 python flasky.py runserver
-# Shell 用于自动导入特定的对象 python flasky.py shell
 from flask_script import Manager, Server, Shell
 
 app = Flask(__name__)
 manager = Manager(app)
-# 添加命令参数，相当于 runserver host='0.0.0.0'
-manager.add_command('runserver', Server(host='0.0.0.0'))
 
 
+# 添加命令行命令
+# Server用于指定server的host等参数 python flasky.py runserver
+# Shell 用于自动导入特定的对象 python flasky.py shell
 def make_shell_context():
     return dict(app=app)
 manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('runserver', Server(host='0.0.0.0'))
 
 
 @app.route('/')
@@ -49,6 +49,5 @@ def page_not_found2(error):
 
 
 if __name__ == '__main__':
-    # host设置为0.0.0.0 局域网通过ip地址访问
     manager.run()
-    # 通过命令 python flask.py runserver 运行服务
+    # 现在通过命令 python flask.py runserver 运行服务
