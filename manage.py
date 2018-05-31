912748a62a83962b753e485d7ea2a2c6b67d9cd8
willis <liuzwill3@163.com>
2018-05-31 17:12:16 +0800

    re migrate and requirements

--- manage.py
+++ manage.py
@@ -43,9 +43,9 @@ manager.add_command('runserver', Server(host='0.0.0.0', port=5000))
 manager.add_command('db', MigrateCommand)
 
 
-@app.cli.command()
-def runserver():
-    app.run(host='0.0.0.0', port=5000)
+#@app.cli.command()
+#def runserver():
+#    app.run(host='0.0.0.0', port=5000)
 
 
 @app.cli.command()
